a83852f26aed89998f180f730d759ea5f41be4c8
Ali Behbahani <dr.ali.behbahani18@gmail.com>
2021-07-29 18:33:11 +0300

    signin

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -3,12 +3,27 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 /* Component */
 import Home from "../Home";
+import Signin from "../authentication/Signin";
 
 const Stack = createStackNavigator();
+
 export default RootNavigator = () => {
   return (
     <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Signin"
+        component={Signin}
+        options={{
+          headerShown: false,
+        }}
+      />
     </Stack.Navigator>
   );
 };
