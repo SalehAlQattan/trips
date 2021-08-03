cb8b9d0aeb4192f99ff42cf3e86e5ac8a7d52ab3
Ali Behbahani <dr.ali.behbahani18@gmail.com>
2021-08-03 20:09:14 +0300

    update image

--- components/profile/myProfile/update/UpdateUser.js
+++ components/profile/myProfile/update/UpdateUser.js
@@ -2,6 +2,8 @@
 import React, { useState } from "react";
 import { observer } from "mobx-react";
 import { Button } from "native-base";
+import { Image } from "react-native";
+import * as DocumentPicker from "expo-document-picker";
 //stores
 import profileStore from "../../../../stores/profileStore";
 //styles
@@ -12,10 +14,34 @@ const UpdateUser = ({ route, navigation }) => {
   const { checkId } = route.params;
   const [profile, setProfile] = useState({
     bio: user.bio,
-    // title: item.title,
-    // description: item.description ? item.description : '',
-    // image: item.image ? item.image : '',
+    image: { uri: user.image ? user.image : "" },
   });
+  const [doc, setDoc] = useState();
+  const pickDocument = async () => {
+    try {
+      let result = await DocumentPicker.getDocumentAsync({
+        type: "*/*",
+        copyToCacheDirectory: true,
+      }).then((response) => {
+        if (response.type == "success") {
+          let { name, size, uri } = response;
+          let nameParts = name.split(".");
+          let fileType = nameParts[nameParts.length - 1];
+          var fileToUpload = {
+            name: name,
+            size: size,
+            uri: uri,
+            type: "application/" + fileType,
+          };
+          setDoc(fileToUpload);
+          setProfile({ ...profile, image: fileToUpload });
+        }
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const handleUpdate = () => {
     /* update the user */
     profileStore.updateItem(profile, checkId);
@@ -30,6 +56,11 @@ const UpdateUser = ({ route, navigation }) => {
         onChangeText={(bio) => setProfile({ ...profile, bio })}
         value={profile.bio}
       />
+      <Image
+        source={{ uri: profile.image.uri }}
+        style={{ width: 200, height: 200 }}
+      />
+      <Button onPress={pickDocument}>Update your image</Button>
       <Button onPress={handleUpdate}>Update</Button>
     </>
   );
